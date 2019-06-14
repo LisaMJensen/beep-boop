fa44bc2bd66abd162eabc3e22382f9eba4bdcd16
Lisa Jensen <leese_38@yahoo.com>
2019-06-14 08:52:00 -0700

    Add HTML form and button, add initial JS ready function and defined initial variables

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//User Interface Logic
+$(document).ready(function() {
+  $("form#roboForm").submit(function(event) {
+    event.preventDefault();
+
+    var userNum = $("input#userInput").val();
+    $("#initialNum").text(userNum);
+
+
+
+
+
+
+//Business Logic
+  var beep = "Beep!"
+  var boop = "Boop!"
+  var sorryDave ="I'm sorry, Dave. I'm afraid I can't do that."
+
+});
+});

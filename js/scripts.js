af51136b77d4755428a0d3f716182b5998f11055
Lisa Jensen <leese_38@yahoo.com>
2019-06-14 14:40:25 -0700

    add new attempt at using string to replace digits with words

--- js/scripts.js
+++ js/scripts.js
@@ -12,20 +12,26 @@ $(document).ready(function() {
 
 
 //Business Logic
-  var beep = "Beep! "
-  var boop = "Boop! "
-  var sorryDave ="I'm sorry, Dave. I'm afraid I can't do that. "
-
+  var beep = " Beep! "
+  var boop = " Boop! "
+  var sorryDave =" I'm sorry, Dave. I'm afraid I can't do that. "
+debugger;
   var numArray = [];
   for (var i = 0; i <= userNum; i++) {
     numArray.push(i);
     var rangeString = numArray.toString();
   //  alert(rangeString);
+}
 
+  if (rangeString.includes("3")) {
+    var result = numArray.replace(/"3"/,sorryDave);
+  } else if (rangeString.includes("2")){
+    var result = numArray.replace(/"2"/,boop);
+  } else if (rangeString.includes("2")){
+  var result = numArray.replace(/"1"/,beep);
+} else var result = numArray.push(i);
 
-
-
-
+alert(result)
 
 
 });

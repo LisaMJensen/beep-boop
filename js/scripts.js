8b77e8570d6914d8288459d65b07db0c69ad0ee2
Lisa Jensen <leese_38@yahoo.com>
2019-06-14 13:43:04 -0700

    not add any major changes - still trying to brainstorm next code to write

--- js/scripts.js
+++ js/scripts.js
@@ -12,18 +12,18 @@ $(document).ready(function() {
 
 
 //Business Logic
-  var beep = "Beep!"
-  var boop = "Boop!"
-  var sorryDave ="I'm sorry, Dave. I'm afraid I can't do that."
+  var beep = "Beep! "
+  var boop = "Boop! "
+  var sorryDave ="I'm sorry, Dave. I'm afraid I can't do that. "
 
   var numArray = [];
-
   for (var i = 0; i <= userNum; i++) {
     numArray.push(i);
-  }
+    var rangeString = numArray.toString();
+  //  alert(rangeString);
 
-  var rangeString = numArray.toString();
-  alert(rangeString);
+
+
 
 
 
